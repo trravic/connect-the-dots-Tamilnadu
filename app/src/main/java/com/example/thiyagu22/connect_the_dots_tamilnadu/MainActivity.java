3a1af8a703d9280da30473b0575f3fd00e54e56b
THIYAGARAJAN <thiyagarajanravi22@gmail.com>
2018-09-18 21:04:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/thiyagu22/connect_the_dots_tamilnadu/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.thiyagu22.connect_the_dots_tamilnadu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
